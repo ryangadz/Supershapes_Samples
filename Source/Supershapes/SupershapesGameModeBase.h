09095877c8c987344a5c58889affdc3ee378ca49
ryangadz@msn.com <ryangadz@msn.com>
2018-06-04 11:20:22 -0700

    migrated project from startup project

---
+++ Source/Supershapes/SupershapesGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SupershapesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPERSHAPES_API ASupershapesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
